0a89ed8243e18e8e7fc57a2df4907a5de33de70c
caioqf <caio.faria@realizati.com.br>
2022-05-27 10:25:16 -0300

    login através da pagina do keycloak e validação no vue

--- front/vue-login/src/App.vue
+++ front/vue-login/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
-    <v-app-bar app dark color="blue">
-      <v-toolbar-title>Ar-Online</v-toolbar-title>
+    <v-app-bar app dark color="green">
+      <v-toolbar-title class="mx-auto">Ar-Online</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn text rounded>Home</v-btn>
       <v-btn text rounded>Login</v-btn>
@@ -9,68 +9,22 @@
 
     <v-main>
       <v-card width="500" class="mx-auto mt-9">
-        <v-card-title>Área de Login</v-card-title>
-        <v-card-text>
-          <v-text-field
-            v-model="usernameForm"
-            id="username"
-            label="Usuário"
-            prepend-icon="mdi-account-circle"
-          ></v-text-field>
-          <v-text-field
-            v-model="passwordForm"
-            id="password"
-            label="Senha"
-            :type="showPassword ? 'text' : 'password'"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-          />
-        </v-card-text>
+        <v-card-title>Área Privada!</v-card-title>
+        <v-img
+          max-height="300"
+          max-width="300"
+          class="mx-auto"
+          alt="Vue logo"
+          src="../src/assets/logo.png"
+        />
         <v-divider></v-divider>
         <v-card-actions>
-          <v-btn @click="submitLogin()" color="success">Logar</v-btn>
-          <v-btn color="info">Registrar</v-btn>
+          <v-btn class="mx-auto mt-9" @click="submitLogin()" color="success"
+            >Logout</v-btn
+          >
         </v-card-actions>
-      </v-card>
-      <v-divider></v-divider>
-      <v-card>
-        <p>{{ info }}</p>
       </v-card>
     </v-main>
   </v-app>
 </template>
-
-<script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-      showPassword: false,
-      info: null,
-      usernameForm: "",
-      passwordForm: "",
-    };
-  },
-  methods: {
-    submitLogin() {
-      axios
-        .post("http://localhost:3000/login", {
-          username: this.usernameForm,
-          password: this.passwordForm,
-        })
-        .then((response) => {
-          this.info = {
-            "Access-Token": response.data.access_token,
-            "ID Token": response.data.id_token,
-          };
-        })
-        .catch((error) => {
-          alert(error);
-        });
-    },
-  },
-};
-</script>
-
 <style lang="scss" scoped></style>
--- front/vue-login/src/main.js
+++ front/vue-login/src/main.js
@@ -1,10 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import Keycloak from 'keycloak-js'
 
 Vue.config.productionTip = false
 
-new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+let initConfig = {
+  url: 'http://localhost:8080/auth',
+  clientId: 'vue',
+  realm: 'aronline',
+  onLoad: 'login-required',
+}
+
+let keycloak = new Keycloak(initConfig);
+
+keycloak.init({onLoad: initConfig.onLoad}).then(auth => {
+  if (!auth){
+    window.location.reload();
+  } else {
+    console.log(auth);
+    new Vue({
+      vuetify,
+      render: h => h(App, {props: {keycloak: keycloak}}),
+    }).$mount('#app')
+  }
+}).catch((e) => {
+  alert(`Failure: ${e}`)
+})
+
+Vue.prototype.$keycloak = keycloak
